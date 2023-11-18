9734e1872ce720ed3f0bc0ec56b317d422928388
Marcin Radoszewski <moriturius@gmail.com>
2023-11-18 09:32:30 +0100

    Opened module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
-module gh.marad.chi.compiler {
+open module gh.marad.chi.compiler {
     requires java.base;
     requires transitive kotlin.stdlib;
     requires org.jgrapht.core;
-    exports gh.marad.chi;
     exports gh.marad.chi.core;
     exports gh.marad.chi.core.parser;
     exports gh.marad.chi.core.analyzer;
