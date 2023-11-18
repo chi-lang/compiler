673e3467b4c9828f0950065aa56a8b30e6acc325
Marcin Radoszewski <moriturius@gmail.com>
2023-11-18 08:52:12 +0100

    Experiment with module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module gh.marad.chi.compiler {
     requires java.base;
     requires transitive kotlin.stdlib;
     requires org.jgrapht.core;
+.
+    exports gh;
+    exports gh.marad;
+    exports gh.marad.chi;
     exports gh.marad.chi.core;
     exports gh.marad.chi.core.parser;
     exports gh.marad.chi.core.analyzer;
