ee4d074cf7a08d64030221cfa59f6a545d345c5f
Marcin Radoszewski <moriturius@gmail.com>
2024-01-06 16:29:11 +0100

    Update default expression visitor

--- src/main/kotlin/gh/marad/chi/core/expressionast/DefaultExpressionVisitor.kt
+++ src/main/kotlin/gh/marad/chi/core/expressionast/DefaultExpressionVisitor.kt
@@ -5,133 +5,78 @@ import gh.marad.chi.core.*
 interface DefaultExpressionVisitor : ExpressionVisitor {
     override fun visit(expr: Expression) = expr.accept(this)
 
-    override fun visitDefineVariantType(defineVariantType: DefineVariantType) { visit(defineVariantType) }
+    override fun visitDefineVariantType(defineVariantType: DefineVariantType) =
+        defineVariantType.children().forEach { visit(it) }
 
-    override fun visitAtom(atom: Atom) { visit(atom) }
+    override fun visitAtom(atom: Atom) =
+        atom.children().forEach { visit(it) }
 
-    override fun visitInterpolatedString(interpolatedString: InterpolatedString) {
-        visit(interpolatedString)
-        for (part in interpolatedString.parts) {
-            part.accept(this)
-        }
-    }
+    override fun visitInterpolatedString(interpolatedString: InterpolatedString) =
+        interpolatedString.children().forEach { visit(it) }
 
-    override fun visitVariableAccess(variableAccess: VariableAccess) { visit(variableAccess) }
+    override fun visitVariableAccess(variableAccess: VariableAccess) =
+        variableAccess.children().forEach { visit(it) }
 
-    override fun visitFieldAccess(fieldAccess: FieldAccess) {
-        fieldAccess.receiver.accept(this)
-        visit(fieldAccess)
-    }
+    override fun visitFieldAccess(fieldAccess: FieldAccess) =
+        fieldAccess.children().forEach { visit(it) }
 
-    override fun visitFieldAssignment(fieldAssignment: FieldAssignment) {
-        fieldAssignment.receiver.accept(this)
-        fieldAssignment.value.accept(this)
-        visit(fieldAssignment)
-    }
+    override fun visitFieldAssignment(fieldAssignment: FieldAssignment) =
+        fieldAssignment.children().forEach { visit(it) }
 
-    override fun visitAssignment(assignment: Assignment) {
-        assignment.value.accept(this)
-        visit(assignment)
-    }
+    override fun visitAssignment(assignment: Assignment) =
+        assignment.children().forEach { visit(it) }
 
-    override fun visitNameDeclaration(nameDeclaration: NameDeclaration) {
-        nameDeclaration.value.accept(this)
-        visit(nameDeclaration)
-    }
+    override fun visitNameDeclaration(nameDeclaration: NameDeclaration) =
+        nameDeclaration.children().forEach { visit(it) }
 
-    override fun visitGroup(group: Group) {
-        group.value.accept(this)
-        visit(group)
-    }
+    override fun visitGroup(group: Group) =
+        group.children().forEach { visit(it) }
 
-    override fun visitFn(fn: Fn) {
-        fn.body.accept(this)
-        visit(fn)
-    }
+    override fun visitFn(fn: Fn) =
+        fn.children().forEach { visit(it) }
 
-    override fun visitBlock(block: Block) {
-        for (expression in block.body) {
-            expression.accept(this)
-        }
-        visit(block)
-    }
+    override fun visitBlock(block: Block) =
+        block.children().forEach { visit(it) }
 
-    override fun visitFnCall(fnCall: FnCall) {
-        fnCall.function.accept(this)
-        for (parameter in fnCall.parameters) {
-            parameter.accept(this)
-        }
-        visit(fnCall)
-    }
+    override fun visitFnCall(fnCall: FnCall) =
+        fnCall.children().forEach { visit(it) }
 
-    override fun visitIfElse(ifElse: IfElse) {
-        ifElse.condition.accept(this)
-        ifElse.thenBranch.accept(this)
-        ifElse.elseBranch?.accept(this)
-        visit(ifElse)
-    }
+    override fun visitIfElse(ifElse: IfElse) =
+        ifElse.children().forEach { visit(it) }
 
-    override fun visitInfixOp(infixOp: InfixOp) {
-        infixOp.left.accept(this)
-        infixOp.right.accept(this)
-        visit(infixOp)
-    }
+    override fun visitInfixOp(infixOp: InfixOp) =
+        infixOp.children().forEach { visit(it) }
 
-    override fun visitPrefixOp(prefixOp: PrefixOp) {
-        prefixOp.expr.accept(this)
-        visit(prefixOp)
-    }
+    override fun visitPrefixOp(prefixOp: PrefixOp) =
+        prefixOp.children().forEach { visit(it) }
 
-    override fun visitCast(cast: Cast) {
-        cast.expression.accept(this)
-        visit(cast)
-    }
+    override fun visitCast(cast: Cast) =
+        cast.children().forEach { visit(it) }
 
-    override fun visitWhileLoop(whileLoop: WhileLoop) {
-        whileLoop.condition.accept(this)
-        whileLoop.loop.accept(this)
-        visit(whileLoop)
-    }
+    override fun visitWhileLoop(whileLoop: WhileLoop) =
+        whileLoop.children().forEach { visit(it) }
 
-    override fun visitBreak(arg: Break) {
-        visit(arg)
-    }
+    override fun visitBreak(arg: Break) =
+        arg.children().forEach { visit(it) }
 
-    override fun visitContinue(arg: Continue) {
-        visit(arg)
-    }
+    override fun visitContinue(arg: Continue) =
+        arg.children().forEach { visit(it) }
 
-    override fun visitIndexOperator(indexOperator: IndexOperator) {
-        indexOperator.variable.accept(this)
-        indexOperator.index.accept(this)
-        visit(indexOperator)
-    }
+    override fun visitIndexOperator(indexOperator: IndexOperator) =
+        indexOperator.children().forEach { visit(it) }
 
-    override fun visitIndexedAssignment(indexedAssignment: IndexedAssignment) {
-        indexedAssignment.variable.accept(this)
-        indexedAssignment.index.accept(this)
-        indexedAssignment.value.accept(this)
-        visit(indexedAssignment)
-    }
+    override fun visitIndexedAssignment(indexedAssignment: IndexedAssignment) =
+        indexedAssignment.children().forEach { visit(it) }
 
-    override fun visitIs(arg: Is) {
-        arg.value.accept(this)
-        visit(arg)
-    }
+    override fun visitIs(arg: Is) =
+        arg.children().forEach { visit(it) }
 
-    override fun visitEffectDefinition(effectDefinition: EffectDefinition) {
-        visit(effectDefinition)
-    }
+    override fun visitEffectDefinition(effectDefinition: EffectDefinition) =
+        effectDefinition.children().forEach { visit(it) }
 
-    override fun visitHandle(handle: Handle) {
-        handle.body.accept(this)
-        for (case in handle.cases) {
-            case.body.accept(this)
-        }
-        visit(handle)
-    }
+    override fun visitHandle(handle: Handle) =
+        handle.children().forEach { visit(it) }
 
-    override fun visitReturn(arg: Return) {
-        visit(arg)
-    }
+    override fun visitReturn(arg: Return) =
+        arg.children().forEach { visit(it) }
 }
