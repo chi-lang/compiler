123ed48112fe802d868c2b3e47d3870706c44625
Marcin Radoszewski <moriturius@gmail.com>
2024-01-06 16:33:22 +0100

    Simplify parser

--- src/main/kotlin/gh/marad/chi/core/Parser.kt
+++ src/main/kotlin/gh/marad/chi/core/Parser.kt
@@ -3,20 +3,17 @@ package gh.marad.chi.core
 import gh.marad.chi.core.analyzer.Message
 import gh.marad.chi.core.antlr.ChiLexer
 import gh.marad.chi.core.antlr.ChiParser
-import gh.marad.chi.core.expressionast.generateExpressionsFromParsedProgram
-import gh.marad.chi.core.expressionast.internal.convertImportDefinition
-import gh.marad.chi.core.expressionast.internal.convertPackageDefinition
-import gh.marad.chi.core.namespace.GlobalCompilationNamespace
 import gh.marad.chi.core.parser.ChiSource
 import gh.marad.chi.core.parser.ParserVisitor
+import gh.marad.chi.core.parser.readers.ParseProgram
 import gh.marad.chi.core.parser.readers.ProgramReader
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.DefaultErrorStrategy
 
-internal fun parseProgram(source: String, namespace: GlobalCompilationNamespace): Pair<Program, List<Message>> {
+internal fun parseSource(source: ChiSource): Pair<ParseProgram, List<Message>> {
     val errorListener = MessageCollectingErrorListener()
-    val charStream = CharStreams.fromString(source)
+    val charStream = CharStreams.fromString(source.code)
     val lexer = ChiLexer(charStream)
     lexer.removeErrorListeners()
     lexer.addErrorListener(errorListener)
@@ -25,17 +22,7 @@ internal fun parseProgram(source: String, namespace: GlobalCompilationNamespace)
     parser.errorHandler = DefaultErrorStrategy()
     parser.removeErrorListeners()
     parser.addErrorListener(errorListener)
-    val chiSource = ChiSource(source)
-    val visitor = ParserVisitor(chiSource)
-    val parseResult = parser.program()
-    val program = if (errorListener.getMessages().isNotEmpty()) {
-        Program(null, emptyList(), emptyList())
-    } else {
-        val parsedProgram = ProgramReader.read(visitor, chiSource, parseResult)
-        generateExpressionsFromParsedProgram(parsedProgram, namespace)
-    }
-    return Pair(
-        program,
-        errorListener.getMessages()
-    )
+    val visitor = ParserVisitor(source)
+    val program = ProgramReader.read(visitor, source, parser.program())
+    return Pair(program, errorListener.getMessages())
 }
