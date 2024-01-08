1476db690892eb77365ee178b2345a471ef4107d
Marcin Radoszewski <moriturius@gmail.com>
2024-01-08 16:27:21 +0100

    Fix simple conversion tests

--- src/test/kotlin/gh/marad/chi/core/expressionast/internal/SimpleConversionsKtTest.kt
+++ src/test/kotlin/gh/marad/chi/core/expressionast/internal/SimpleConversionsKtTest.kt
@@ -4,8 +4,6 @@ import gh.marad.chi.core.*
 import gh.marad.chi.core.parser.readers.*
 import gh.marad.chi.core.types.Types
 import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.nulls.shouldBeNull
-import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
@@ -14,18 +12,17 @@ import org.junit.jupiter.api.Test
 class SimpleConversionsKtTest {
     @Test
     fun `generating simple atoms`() {
-        convertAtom(LongValue(10, testSection)).shouldBeAtom("10", Types.int, testSection)
-        convertAtom(FloatValue(0.5f, testSection)).shouldBeAtom("0.5", Types.float, testSection)
-        convertAtom(BoolValue(true, testSection)).shouldBeAtom("true", Types.bool, testSection)
-        convertAtom(BoolValue(false, testSection)).shouldBeAtom("false", Types.bool, testSection)
-        convertAtom(StringValue("test", testSection)).shouldBeAtom("test", Types.string, testSection)
+        convertAst(LongValue(10, testSection)).shouldBeAtom("10", Types.int, testSection)
+        convertAst(FloatValue(0.5f, testSection)).shouldBeAtom("0.5", Types.float, testSection)
+        convertAst(BoolValue(true, testSection)).shouldBeAtom("true", Types.bool, testSection)
+        convertAst(BoolValue(false, testSection)).shouldBeAtom("false", Types.bool, testSection)
+        convertAst(StringValue("test", testSection)).shouldBeAtom("test", Types.string, testSection)
     }
 
     @Test
     fun `generating interpolated string`() {
         // when
-        val result = convertInterpolatedString(
-            defaultContext(),
+        val result = convertAst(
             ParseInterpolatedString(
                 section = testSection,
                 parts = listOf(
@@ -48,61 +45,41 @@ class SimpleConversionsKtTest {
 
     @Test
     fun `string text in interpolation should be converted to simple string atom`() {
-        convertStringText(StringText("test", testSection))
+        convertAst(StringText("test", testSection))
             .shouldBeAtom("test", Types.string, testSection)
     }
 
     @Test
     fun `code interpolations should be converted and cast to string`() {
-        convertInterpolation(defaultContext(), ParseInterpolation(LongValue(10), testSection))
-            .shouldBeTypeOf<Cast>() should {
+        convertAst(
+            ParseInterpolation(LongValue(10), testSection)
+        ).shouldBeTypeOf<Cast>() should {
             it.targetType shouldBe Types.string
             it.expression.shouldBeAtom("10", Types.int)
         }
     }
 
     @Test
-    fun `converting null package definition should produce null value`() {
-        convertPackageDefinition(null).shouldBeNull()
-    }
-
-    @Test
-    fun `package definition conversion`() {
-        val result = convertPackageDefinition(
-            ParsePackageDefinition(
-                ModuleName("my.mod", sectionA),
-                PackageName("my.pkg", sectionB),
-                sectionC
-            )
-        )
-
-        result.shouldNotBeNull().shouldBeTypeOf<Package>() should {
-            it.moduleName shouldBe "my.mod"
-            it.packageName shouldBe "my.pkg"
-        }
-    }
-
-    @Test
     fun `block conversion`() {
-        convertBlock(defaultContext(), ParseBlock(listOf(LongValue(10)), testSection)) should {
-            it.body shouldHaveSize 1
-            it.body[0].shouldBeAtom("10", Types.int)
-            it.sourceSection shouldBe testSection
-        }
+        convertAst(ParseBlock(listOf(LongValue(10)), testSection))
+            .shouldBeTypeOf<Block>().should {
+                it.body shouldHaveSize 1
+                it.body[0].shouldBeAtom("10", Types.int)
+                it.sourceSection shouldBe testSection
+            }
     }
 
     @Test
     fun `convert binary operator`() {
         // when
-        val result = convertBinaryOp(
-            defaultContext(),
+        val result = convertAst(
             ParseBinaryOp(
                 op = "generic operation",
                 left = StringValue("hello"),
                 right = LongValue(20),
                 section = testSection
             )
-        )
+        ).shouldBeTypeOf<InfixOp>()
 
         // then
         result.op shouldBe "generic operation"
@@ -114,14 +91,13 @@ class SimpleConversionsKtTest {
     @Test
     fun `convert cast`() {
         // when
-        val result = convertCast(
-            defaultContext(),
+        val result = convertAst(
             ParseCast(
                 value = LongValue(10),
                 typeRef = TypeNameRef("string", sectionA),
                 section = sectionB
             )
-        )
+        ).shouldBeTypeOf<Cast>()
 
         // then
         result.expression.shouldBeAtom("10", Types.int)
@@ -131,10 +107,12 @@ class SimpleConversionsKtTest {
 
     @Test
     fun `convert not`() {
-        convertNot(defaultContext(), ParseNot(BoolValue(true), testSection)).should {
-            it.op shouldBe "!"
-            it.expr.shouldBeAtom("true", Types.bool)
-            it.sourceSection shouldBe testSection
-        }
+        convertAst(ParseNot(BoolValue(true), testSection))
+            .shouldBeTypeOf<PrefixOp>()
+            .should {
+                it.op shouldBe "!"
+                it.expr.shouldBeAtom("true", Types.bool)
+                it.sourceSection shouldBe testSection
+            }
     }
 }
