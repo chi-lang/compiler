ba2dc4366c6ec773ad7703fd8a6b962edb9d5aa1
Marcin Radoszewski <moriturius@gmail.com>
2024-04-06 14:01:14 +0200

    Add docopt

---
+++ src/main/java/org/docopt/OneOrMore.java
@@ -0,0 +1,52 @@
+package org.docopt;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.docopt.Python.list;
+
+final class OneOrMore extends BranchPattern {
+
+	public OneOrMore(final List<? extends Pattern> children) {
+		super(children);
+	}
+
+	@Override
+	protected MatchResult match(final List<LeafPattern> left,
+			List<LeafPattern> collected) {
+		assert getChildren().size() == 1;
+
+		if (collected == null) {
+			collected = list();
+		}
+
+		List<LeafPattern> l = left;
+		List<LeafPattern> c = collected;
+		List<LeafPattern> l_ = null;
+		final boolean matched = true;
+		int times = 0;
+
+		while (matched) {
+			final MatchResult m = getChildren().get(0).match(l, c);
+
+			l = m.getLeft();
+			c = m.getCollected();
+
+			if (m.matched()) {
+				times++;
+			}
+
+			if (Objects.equals(l, l_)) {
+				break;
+			}
+
+			l_ = l;
+		}
+
+		if (times >= 1) {
+			return new MatchResult(true, l, c);
+		}
+
+		return new MatchResult(false, left, collected);
+	}
+}
