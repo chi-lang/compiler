c9b5f6d4bd1f725e750ddbb291e002804275b380
Marcin Radoszewski <moriturius@gmail.com>
2024-04-26 15:55:38 +0200

    Fix linux native image

--- native/build.sh
+++ native/build.sh
@@ -6,11 +6,11 @@ $GRAALVM_HOME/bin/native-image \
     --initialize-at-run-time=party.iroiro \
     --no-fallback \
     -H:+UnlockExperimentalVMOptions \
-    -H:ReflectionConfigurationFiles=config/reflect-config.json \
-    -H:JNIConfigurationFiles=config/jni-config.json \
-    -H:DynamicProxyConfigurationFiles=config/proxy-config.json \
-    -H:SerializationConfigurationFiles=config/serialization-config.json \
-    -H:ResourceConfigurationFiles=config/resource-config.json \
+    -H:ReflectionConfigurationFiles=config/linux/reflect-config.json \
+    -H:JNIConfigurationFiles=config/linux/jni-config.json \
+    -H:DynamicProxyConfigurationFiles=config/linux/proxy-config.json \
+    -H:SerializationConfigurationFiles=config/linux/serialization-config.json \
+    -H:ResourceConfigurationFiles=config/linux/resource-config.json \
     -cp build/libs/chi-all.jar \
     gh.marad.chi.MainKt \
     chi
