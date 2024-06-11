6a81d73130ffb7c2ef809bef49826bed5931eb98
Marcin Radoszewski <moriturius@gmail.com>
2024-06-11 19:21:54 +0200

    Try different configuration of native-image agent for Windows

--- native/run_with_agent.bat
+++ native/run_with_agent.bat
@@ -1,4 +1,7 @@
 @echo off
 
 
-%GRAALVM_HOME%\bin\java -agentlib:native-image-agent=config-output-dir=./config,config-write-period-secs=30 -cp build\libs\chi-all.jar gh.marad.chi.MainKt native/seed.chi  %*
+%GRAALVM_HOME%\bin\java ^
+    -agentlib:native-image-agent=config-output-dir=./config,config-write-period-secs=30,config-write-initial-delay-secs=2 ^
+    -cp build\libs\chi-all.jar ^
+    gh.marad.chi.MainKt native/seed.chi  %*
