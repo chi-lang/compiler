4a378ee7483412f5756f7ea2885737063d293b8a
Marcin Radoszewski <moriturius@gmail.com>
2024-06-11 16:20:30 +0200

    Pass windows native build script arguments to chi

--- native/run_with_agent.bat
+++ native/run_with_agent.bat
@@ -1,4 +1,4 @@
 @echo off
 
 
-%GRAALVM_HOME%\bin\java -agentlib:native-image-agent=config-output-dir=./config,config-write-period-secs=30 -cp build\libs\chi-all.jar gh.marad.chi.MainKt
+%GRAALVM_HOME%\bin\java -agentlib:native-image-agent=config-output-dir=./config,config-write-period-secs=30 -cp build\libs\chi-all.jar gh.marad.chi.MainKt %*
