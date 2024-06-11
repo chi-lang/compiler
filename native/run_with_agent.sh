1f6638a8aa77c44b953849cbe8b8947ec0ab8e11
Marcin Radoszewski <moriturius@gmail.com>
2024-06-11 07:10:11 +0200

    Load stdlib for native agent run

--- native/run_with_agent.sh
+++ native/run_with_agent.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-$GRAALVM_HOME/bin/java -agentlib:native-image-agent=config-output-dir=./config/linux,config-write-period-secs=30 -cp build/libs/chi-all.jar gh.marad.chi.MainKt native/seed.chi
+$GRAALVM_HOME/bin/java \
+  -agentlib:native-image-agent=config-output-dir=./config/linux,config-write-period-secs=30 \
+  -cp build/libs/chi-all.jar \
+  gh.marad.chi.MainKt \
+  native/seed.chi $@
